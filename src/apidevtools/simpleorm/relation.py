c9678a9e1d618ce48cf238c7b23aaf87f9ebb384
Roman Lysychkin <cxllmerichie@gmail.com>
2023-02-27 16:43:33 +0100

    circular import fixed in relation.py

--- src/apidevtools/simpleorm/relation.py
+++ src/apidevtools/simpleorm/relation.py
@@ -1,14 +1,12 @@
 from typing import Any
 
-from .types import SchemaType
-
 
 class Relation:
-    def __init__(self, tablename: str, where: dict[str, Any], ext_schema_t: SchemaType, fieldname: str,
-                 rel_schema_t: SchemaType = dict, columns: list[str] = None):
+    def __init__(self, tablename: str, where: dict[str, Any], ext_schema_t: 'SchemaType', fieldname: str,
+                 rel_schema_t: 'SchemaType' = dict, columns: list[str] = None):
         self.tablename: str = tablename
         self.where: dict[str, Any] = where
-        self.ext_schema_t: SchemaType = ext_schema_t
+        self.ext_schema_t: 'SchemaType' = ext_schema_t
         self.fieldname: str = fieldname
-        self.rel_schema_t: SchemaType = rel_schema_t
+        self.rel_schema_t: 'SchemaType' = rel_schema_t
         self.columns: list[str] = columns if columns else ['*']
