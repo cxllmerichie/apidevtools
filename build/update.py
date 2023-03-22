806fd21b4067dbd3ca6c9dfaac4490d871cbfc3d
Roman Lysychkin <cxllmerichie@gmail.com>
2023-03-22 01:07:13 +0100

    build.update security issue fixed

--- build/update.py
+++ build/update.py
@@ -1,3 +1,4 @@
+import logging
 import dotenv
 import subprocess
 import tomli
@@ -15,11 +16,11 @@ if __name__ == '__main__':
     PYPI_PASSWORD: str = os.getenv('PYPI_PASSWORD')
 
     commands = [
-        'python -m build ../ --outdir ../dist',
-        f'python -m twine upload ../dist/apidevtools-{VERSION}* -u{PYPI_USERNAME} -p{PYPI_PASSWORD}'
+        {'orig': 'python -m build ../ --outdir ../dist', 'alt': 'Building...'},
+        {'orig': f'python -m twine upload ../dist/apidevtools-{VERSION}* -u{PYPI_USERNAME} -p{PYPI_PASSWORD}', 'alt': 'Uploading...'}
     ]
     for command in commands:
-        print(f'{command}')
-        process = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        logging.warning(command['alt'])
+        process = subprocess.Popen(command['orig'], shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
         out, err = process.communicate()
         print(f'{out.decode()}')
